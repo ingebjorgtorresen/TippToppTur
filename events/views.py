8f782037244a27a8731a595d02a5f05811b6a7e5
Sebastian Hegreberg <sebheg77@gmail.com>
2022-02-15 14:34:06 +0100

    feat (#19) - Add model view to trips

--- events/views.py
+++ events/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
 def trips(request):
     return render(request, 'landing_page/trips.html')
+
+def tripstest(request):
+    event = Event.objects.all()
+    context = {'tittel': event,
+    'beskrivelse': event}
+    return render(request, 'landing_page/trips.html', context)
