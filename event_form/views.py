0661ac4a5e438bd124336802272ef0a46b05d0bd
Thomas <thomawj@stud.ntnu.no>
2022-02-16 21:12:36 +0100

    feat(#16) - Add templates

--- event_form/views.py
+++ event_form/views.py
@@ -4,7 +4,7 @@ from events.models import Event
 # Create your views here.
 
 def event_form(request):
-    return render(request, 'event_form.html')
+    return render(request, 'event_form/event_form.html')
 
 def new_event(request):
     e = Event(tittel=request.POST['title'], dato=request.POST['date'], beskrivelse=request.POST['description'])
