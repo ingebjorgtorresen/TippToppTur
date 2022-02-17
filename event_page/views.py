cf514fee6122b8cb0a12687cb42bb5de0624d95d
thomawj <thomawj@ntnu.no>
2022-02-17 15:10:11 +0100

    feat(#18) - add error handling for events

--- event_page/views.py
+++ event_page/views.py
@@ -3,10 +3,12 @@ from events.models import Event
 
 # Create your views here.
 
-def event_page(request):
+def event_page(request, id):
+    id = request.GET.get('id', '0')
     try:
-        event = Event.objects.get(pk=1)
+        event = Event.objects.get(pk=id)
         context = {
+            'exists': True,
             'title': event.tittel,
             'date': event.dato,
             'arrangør': event.arrangør,
@@ -14,5 +16,8 @@ def event_page(request):
             'description': event.beskrivelse,
         }
     except Event.DoesNotExist:
+        context = {
+            'exists': False,
+        }
         return render(request, 'event_page/event_page.html', context)
     return render(request, 'event_page/event_page.html', context)
