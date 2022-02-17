ba885647d8f6a40f5903b8ad265ef72d20bd2518
thomawj <thomawj@ntnu.no>
2022-02-17 14:38:02 +0100

    feat(#18) - access event from database

--- event_page/views.py
+++ event_page/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from events.models import Event
 
 # Create your views here.
 
 def event_page(request):
-    context = {
-        'title': "tittel",
-        'date': "dato",
-        'destination': "destinasjon",
-        'description': "beskrivelse",
-    }
+    try:
+        event = Event.objects.get(pk=1)
+        context = {
+            'title': event.tittel,
+            'date': event.dato,
+            'destination': "Kommer senere",
+            'description': event.beskrivelse,
+        }
+    except Event.DoesNotExist:
+        return render(request, 'event_page/event_page.html', context)
     return render(request, 'event_page/event_page.html', context)
