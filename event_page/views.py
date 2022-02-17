ff63cbe227c5728ff6e5759cd1df7025db3297ef
thomawj <thomawj@ntnu.no>
2022-02-17 13:34:18 +0100

    feat(#18) - add context for HTML templates

--- event_page/views.py
+++ event_page/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def event_page(request):
-    return render(request, 'event_page/event_page.html')
+    context = {
+        'title': "tittel",
+        'date': "dato",
+        'destination': "destinasjon",
+        'description': "beskrivelse",
+    }
+    return render(request, 'event_page/event_page.html', context)
