9df30cfbb6215c70e2e430eadf57b014b6f9af3a
Ingebjørg Semb Tørresen <ingebsto@stud.ntnu.no>
2022-02-17 16:07:50 +0100

    <feature>:(#19)- add bootstrap to event page

--- event_page/views.py
+++ event_page/views.py
@@ -3,10 +3,10 @@ from events.models import Event
 
 # Create your views here.
 
-def event_page(request, id):
+def event_page(request):
     id = request.GET.get('id', '0')
     try:
-        event = Event.objects.get(pk=id)
+        event = Event.objects.get(pk=1)
         context = {
             'exists': True,
             'title': event.tittel,
