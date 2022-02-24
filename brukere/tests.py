78d7d68e4e423a41797a1e3528ab0865885fd8a4
thomawj <thomawj@ntnu.no>
2022-02-24 14:04:16 +0100

    test(#12) - write test for creating user + event

--- brukere/tests.py
+++ brukere/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from brukere.models import *
+from events.models import Event
 
 # Create your tests here.
+
+class registerTestCase(TestCase):
+    def setUp(self):
+        turgåer = Turgåere.objects.create(username="Test", password="password12321")
+        Event.objects.create(tittel="test", arrangør=turgåer, beskrivelse="test", dato="2022-02-17")
