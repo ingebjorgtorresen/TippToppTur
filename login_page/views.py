2dfc7ee78bb1c799bcda02e0970f8ebcdc581b88
Markus Johansen <markujo@stud.ntnu.no>
2022-02-24 12:25:32 +0100

    feat(#13) - added tests for password and name fields in register form

--- login_page/views.py
+++ login_page/views.py
@@ -1,3 +1,4 @@
+from codecs import register_error
 from django.contrib.auth import login, authenticate
 from django.forms import PasswordInput
 from django.shortcuts import render, redirect
@@ -17,33 +18,86 @@ def login_page(request):
 
 
 def register(request):
-    return render(request, "login_page/newuser2.html")
+    error = request.session.pop("error", None)
+    if error is None:
+        error = ""
+    context = {'error': error}
+    return render(request, "login_page/newuser2.html", context)
 
 def register_user(request):
     username = request.POST["username"]
     password = request.POST["password"]
     password2 = request.POST["password2"]
-    navn = request.POST['navn']
+    name = request.POST['navn']
     epost = request.POST['mail']
 
     noe = Turgåere.objects.filter(username=username)
     if (noe):
+        request.session["error"] = "Brukernavn eksisterer allerede!!"
+        return redirect("register")
+
+    # Test name
+    if name is None or name == " ":
+        request.session["error"] = "Må spesifisere navne!!"
+        return redirect("register")
+
+    names = name.split(" ")
+    if len(name) <= 1:
+        request.session["error"] = "Må spesifisere fornavn og etternavn!!"
+        return redirect("register")
+
+    for name in names:
+        if len(name) < 1:
+            request.session["error"] = "Ugyldig format!!"
+            return redirect("register")
+
+    # Test passwords
+    if (password is None or len(password) < 8):
+        request.session["error"] = "Password is too short!!"
+        return redirect("register")
+
+    contains_lower = False
+    contains_upper = False
+    contains_number = False
+    contains_symbol = False
+    for a in password:
+        if (a.islower()):
+            contains_lower = True
+        if (a.isupper()):
+            contains_upper = True
+        if (a.isnumeric()):
+            contains_number = True
+        if ( "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".__contains__(a)):
+            contains_symbol = True
+    
+    if not contains_upper:
+        request.session["error"] = "Passordet må inneholde minst en stor bokstav!!"
+        return redirect("register")
+
+    if not contains_lower:
+        request.session["error"] = "Passordet må inneholde minst en liten bokstav!!"
         return redirect("register")
     
+    if not contains_symbol:
+        request.session["error"] = "Passordet må inneholde minst ett symbol!!"
+        return redirect("register")
+
+    if not contains_number:
+        request.session["error"] = "Passordet må inneholde minst ett tall!!"
+        return redirect("register")
+
     if (password != password2):
+        request.session["error"] = "Passordene må matche!!"
         return redirect("register")
     
     user = Turgåere(username=username, password=make_password(password))
     user.save()
-
-    #new_user = Turgåere.objects.get_or_create(username=username,is_staff = False)
-    #new_user.set_password(password)
-    #new_user.save()
     
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
         return redirect("home")
     else:
+        request.session["error"] = "Feil i databasen, kontakt systemansvarlig!!"
         return redirect("register")
     
