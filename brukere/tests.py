37258301088b31286b35fa71d8852691ba28dfe0
thomawj <thomawj@ntnu.no>
2022-02-24 14:43:00 +0100

    test(#12) - add tests for error handling

--- brukere/tests.py
+++ brukere/tests.py
@@ -8,5 +8,14 @@ class registerTestCase(TestCase):
     def testRegister(self):
         turgåer = Turgåere.objects.create(username="Test", password="password12321")
         event = Event.objects.create(tittel="test", arrangør=turgåer, beskrivelse="test", dato="2022-02-17")
-        User_registration.objects.create(user_pk=turgåer, event_pk=event)
-        self.assertAlmostEqual(User_registration.objects.filter(user_pk=turgåer, event_pk=event).exists(), True)
+        turgåer.register(event)
+        self.assertEqual(User_registration.objects.filter(user_pk=turgåer, event_pk=event).exists(), True)
+
+    def testErrorHandling(self):
+        turgåer = Turgåere.objects.create(username="Test", password="password12321")
+        event = Event.objects.create(tittel="test", arrangør=turgåer, beskrivelse="test", dato="2022-02-17")
+        self.assertEqual(turgåer.register(event), True)
+        self.assertEqual(turgåer.register(event), False)
+        self.assertEqual(turgåer.unRegister(event), True)
+        self.assertEqual(turgåer.unRegister(event), False)
+        self.assertEqual(User_registration.objects.filter(user_pk=turgåer, event_pk=event).exists(), False)
