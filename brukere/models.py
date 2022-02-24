850295b61c1bfd093aec65de69e220c05a85ead6
thomawj <thomawj@ntnu.no>
2022-02-24 12:57:43 +0100

    feat(#12) - add queries and registration object

--- brukere/models.py
+++ brukere/models.py
@@ -29,7 +29,28 @@ class Turgåere(AbstractUser):
                           max_length=20, blank=False, default=" ")
     telefonnummer = models.CharField(max_length=12, blank=False, default='')
     ferdighetsnivå = models.CharField(choices=experience, blank=False, max_length=20, default=" ")
-    turer = []
+
+    ##Metoden tar inn et event objekt som argument
+    @classmethod
+    def register(cls, event):
+        registration = User_registration(user_pk=cls.pk, event_pk=event.pk)
+        registration.save()
+        return None
+
+    @classmethod
+    def unRegister(cls, event):
+        registration = User_registration.objects.filter(user_pk=cls.pk, event_pk=event.pk)
+        registration.delete()
+        return None
+    
+    @classmethod
+    def isRegistered(cls, event):
+        registration = User_registration.objects.filter(user_pk=cls.pk, event_pk=event.pk)
+        return registration.exists()
 
     def __str__(self):
         return '{}'.format(self.username)
+
+class User_registration(models.Model):
+    user_pk = models.ForeignKey(Turgåere, on_delete=models.CASCADE)
+    event_pk = models.ForeignKey('events.Event', on_delete=models.CASCADE)
