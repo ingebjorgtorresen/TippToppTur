520263e605766a4cdaf502632dc50386c6cb9127
ChristianKartveit <chrigk@stud.ntnu.no>
2022-03-01 15:17:16 +0100

    feat:(#2.6) - fix error when not logged in, and check detailed "tur" page
    Co-authored-by: @thomawj

--- event_page/views.py
+++ event_page/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from events.models import Event
 from brukere.models import Turgåere, User_registration
 
+
 # Create your views here.
 
 def event_page(request):
@@ -16,15 +17,24 @@ def event_page(request):
             'arrangør': event.arrangør,
             'destination': "Kommer senere",
             'description': event.beskrivelse,
-            'id' : id,
+            'id': id,
             'påmeldt': request.user.isRegistered(Event.objects.get(pk=id)),
         }
+    except AttributeError:
+        context = {'exists': True,
+                   'title': event.tittel,
+                   'date': event.dato,
+                   'arrangør': event.arrangør,
+                   'destination': "Kommer senere",
+                   'description': event.beskrivelse,
+                   }
     except Event.DoesNotExist:
         context = {
             'exists': False,
         }
         return render(request, 'event_page/event_page.html', context)
     return render(request, 'event_page/event_page.html', context)
+
 
 def register_event(request):
     id = request.GET.get('id', '0')
@@ -33,8 +43,7 @@ def register_event(request):
         request.user.register(event)
     else:
         request.user.unRegister(event)
-    
+
     link = "../event_page/?id="
     link += id
     return redirect(link)
-        
