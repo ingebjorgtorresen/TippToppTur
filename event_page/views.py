1898b7eb4207b474162be920ea84cb987a58317b
Sebastian Hegreberg <sebheg77@gmail.com>
2022-03-15 13:47:40 +0100

    fix (#42) - Add list of attendees on event details

--- event_page/views.py
+++ event_page/views.py
@@ -10,6 +10,7 @@ def event_page(request):
     id = request.GET.get('id', '0')
     try:
         event = Event.objects.get(pk=id)
+        registrert = User_registration.objects.filter(event_pk = id)
         context = {
             'exists': True,
             'title': event.tittel,
@@ -19,6 +20,7 @@ def event_page(request):
             'description': event.beskrivelse,
             'id': id,
             'påmeldt': request.user.isRegistered(Event.objects.get(pk=id)),
+            'registrert': registrert,
         }
     except AttributeError:
         context = {'exists': True,
@@ -35,7 +37,6 @@ def event_page(request):
         return render(request, 'event_page/event_page.html', context)
     return render(request, 'event_page/event_page.html', context)
 
-
 def register_event(request):
     id = request.GET.get('id', '0')
     event = Event.objects.get(pk=id)
@@ -47,3 +48,4 @@ def register_event(request):
     link = "../event_page/?id="
     link += id
     return redirect(link)
+
