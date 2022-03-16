64a04851fcaa56fa336cbd09f0ead9acefff0c82
ChristianKartveit <chrigk@stud.ntnu.no>
2022-03-16 09:53:53 +0100

    feat:(#41) - make it impossible to create events back in time
    Co-authored-by:

--- event_form/views.py
+++ event_form/views.py
@@ -1,5 +1,4 @@
-import datetime
-
+from datetime import date, datetime
 
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
@@ -10,23 +9,49 @@ from django.utils.timezone import *
 import brukere.models
 from events.models import Event, validate_date_bool
 from brukere import *
+
+
 # Create your views here.
 
 def event_form(request):
-    datoidag = datetime.date.today().__str__()
-    context = {'datoidag':datoidag}
-    return render(request, 'event_form/event_form.html',context)
+    datotime = datetime.now()
+    error = request.session.pop("error", None)
+    if error is None:
+        error = ""
+
+    context = {'datoidag': datotime,
+               'error': error,
+               }
+    return render(request, 'event_form/event_form.html', context)
+
 
 def new_event(request):
-    date = request.POST['date']
-    date_today = datetime.date.today()
-    date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
-    print(date < date_today)
-    if(date < date_today):
+    datoen1 = request.POST['date']
+    datoen = datoen1.split('T')
+    # ['2022-03-01', '15:38']
+    datoenher = datetime.strptime(datoen[0], '%Y-%m-%d').date()
+    # 2022-03-01 -Date objekt
+    date_today = date.today()
+    # 2022-03-15
+    timenow = (datetime.strptime(datoen[1], '%H:%M')).time()
+    # 15:38:00
+    timetoday = (datetime.now().time()).strftime('%H:%M:%S')
+    timetoday = datetime.strptime(timetoday, '%H:%M:%S')
+    timetoday = timetoday.time()
+    # 09:03:34
+
+    if not (datoenher > date_today):
+        request.session["error"] = "Du må velge dato frem i tid!"
         return redirect('event_form')
 
+        # Frem i tid
+    if datoenher == date_today:
+        if timetoday > timenow:
+            request.session["error"] = "Du må velge dato frem i tid!"
+            return redirect('event_form')
+
     e = Event(tittel=request.POST['title'],
-              dato=date,
+              dato=datoen1,
               beskrivelse=request.POST['description'],
               arrangør=request.user.get_full_name())
     e.save()
