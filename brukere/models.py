c1b8733a67fda7203350a59cd5ef2938708775be
ChristianKartveit <chrigk@stud.ntnu.no>
2022-03-17 12:30:05 +0100

    feat:(#45) - added requiredfield for superuser
    Co-authored-by:

--- brukere/models.py
+++ brukere/models.py
@@ -30,6 +30,7 @@ class Turgåere(AbstractUser):
     telefonnummer = models.CharField(max_length=12, blank=False, default='')
     ferdighetsnivå = models.CharField(choices=experience, blank=False, max_length=20, default=" ")
     fødselsdato = models.DateField(verbose_name='Fødselsdato', blank=True, default=None, null=True)
+    REQUIRED_FIELDS = ['fødselsdato']
 
     def fullName(self):
         return self.first_name + " " + self.last_name
