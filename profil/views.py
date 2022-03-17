e7cb78a802f45070a3870701c02445ae0ea3a939
thomawj <thomawj@ntnu.no>
2022-03-17 13:37:52 +0100

    feat(#37) - linked profile to navbar

--- profil/views.py
+++ profil/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def profil(request):
-    pass
+    user = request.user
+    user_auth = user.is_authenticated
+    context = {'user': user_auth, "name": user.username, "view": True}
+    return render(request, 'profil/profil.html', context)
