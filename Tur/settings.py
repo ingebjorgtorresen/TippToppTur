2c79d2c992306d0df9e61ea88aa7ed8c3edda858
Christian Grøttveit Kartveit <chrigk@stud.ntnu.no>
2022-03-29 11:52:18 +0200

    Revert "Revert "Merge branch..."
    This reverts commit 14a9f593fda7d36f0d104e68d751601a30fb3f3e

--- Tur/settings.py
+++ Tur/settings.py
@@ -67,11 +67,12 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'Tur.wsgi.application'
 
--- brukere/models.py
+++ brukere/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
-
 # Create your models here.
 from django.db.models.signals import *
 
@@ -33,22 +32,23 @@ class Turgåere(AbstractUser):
     bedriftsaddresse = models.CharField(default='', max_length=50)
     fødselsdato = models.DateField(verbose_name='Fødselsdato', blank=True, default=None, null=True)
     REQUIRED_FIELDS = ['fødselsdato']
-#Endring
+
+    # Endring
     def fullName(self):
         return self.first_name + " " + self.last_name
 
     def updateUser(self, fullName, email, date):
         splitName = fullName.split(' ')
-        user = Turgåere.objects.filter(pk = self.pk)
-        user.update(first_name = splitName[0], last_name = splitName[1], email = email, fødselsdato = date)
+        user = Turgåere.objects.filter(pk=self.pk)
+        user.update(first_name=splitName[0], last_name=splitName[1], email=email, fødselsdato=date)
         return True
 
     def updateSeriousUser(self, fullName, email):
         splitName = fullName.split(' ')
-        user = Turgåere.objects.filter(pk = self.pk)
-        user.update(first_name = splitName[0], last_name = splitName[1], email = email)
+        user = Turgåere.objects.filter(pk=self.pk)
+        user.update(first_name=splitName[0], last_name=splitName[1], email=email)
         return True
-        
+
     ##Metoden tar inn et event objekt som argument
     def register(self, event):
         if not self.isRegistered(event):
@@ -65,7 +65,7 @@ class Turgåere(AbstractUser):
         else:
             return False
         return True
-    
+
     def isRegistered(self, event):
         registration = User_registration.objects.filter(user_pk=self, event_pk=event)
         return registration.exists()
@@ -76,9 +76,64 @@ class Turgåere(AbstractUser):
     def isSeriøsAktør(self):
         return self.seriøsaktør
 
+    def hasUpvote(self, event):
+        upvote = UpvotePoints.objects.filter(user_pk=self, event_pk=event)
+
+        ableToVote = upvote.filter(points=1)
+        if len(ableToVote) > 0:
+            print("Upvote1")
+            test = upvote.filter(points=-1)
+            print(test)
+            if len(test) > 0:
+                test.delete()
+            return False
+        else:
+            print("Upvote2")
+
+            return True
+
+    def hasDownvote(self, event):
+        upvote = UpvotePoints.objects.filter(user_pk=self, event_pk=event)
+        ableToVote = upvote.filter(points=-1)
+        if len(ableToVote) > 0:
+
+            print("Downvote1")
+            test = upvote.filter(points=1)
+            if len(test) > 0:
+                test.delete()
+            return False
+        else:
+            print("Downvote2")
+            return True
+
+    def upvote(self, event):
+        userupvotes = UpvotePoints.objects.filter(user_pk=self, event_pk=event)
+        upvote = UpvotePoints(user_pk=self, event_pk=event, points=1)
+        upvote.save()
+
+        return True
+
+    def canVote(self, event):
+        return UpvotePoints.objects.filter(user_pk=self, event_pk=event)
+
+    def downvote(self, event):
+        upvote = UpvotePoints(user_pk=self, event_pk=event, points=-1)
+        upvote.save()
+        return True
+
+
 class User_registration(models.Model):
     user_pk = models.ForeignKey(Turgåere, on_delete=models.CASCADE)
     event_pk = models.ForeignKey('events.Event', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}, {}'.format(self.user_pk, self.event_pk.pk)
+
+
+class UpvotePoints(models.Model):
+    user_pk = models.ForeignKey(Turgåere, on_delete=models.CASCADE)
+    event_pk = models.ForeignKey('events.Event', on_delete=models.CASCADE)
+    points = models.IntegerField(blank=False, default=0)
 
     def __str__(self):
         return '{}, {}'.format(self.user_pk, self.event_pk.pk)
